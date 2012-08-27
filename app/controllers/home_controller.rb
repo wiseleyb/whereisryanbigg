2968e61529abbbd06768bebdb5fae2dc2c4193c2
wiseleyb <wiseleyb@gamil.com>
2012-08-28 09:10:40 +1000

    Adding update_twitter ping

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,10 @@ class HomeController < ApplicationController
 		# render :layout => nil
 	end
 
+	def update_twitter
+		tweets = Twitter.user_timeline("ryanbigg", :count => 200)
+		Ryan.from_tweet(tweets)
+		redirect_to root_path
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Whereisryanbigg::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  match '/update_twitter' => 'home#update_twitter'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
